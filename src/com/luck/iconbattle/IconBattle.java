b3ebcefb5b9687cfcfb11837c58f410cdaded3cd
luckily <luck248@163.com>
2013-06-21 17:22:58 +0800

    path

---
+++ src/com/luck/iconbattle/IconBattle.java
@@ -0,0 +1,12 @@
+package com.luck.iconbattle;
+import com.badlogic.gdx.Game;
+
+public class IconBattle extends Game {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
